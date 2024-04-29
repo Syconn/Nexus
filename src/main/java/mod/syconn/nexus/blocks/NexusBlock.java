d792f45a04038a1bf20369f367fe3fd20a39b63c
Syconn <Syconngaming@gmail.com>
2024-04-28 20:51:03 -0400

    NexusBlockRequirement - added in nexus block required to use interface - interface changes texture depending on state - fixed interface datagen so its a faster load

--- src/main/java/mod/syconn/nexus/blocks/NexusBlock.java
+++ src/main/java/mod/syconn/nexus/blocks/NexusBlock.java
@@ -1,11 +1,29 @@
 package mod.syconn.nexus.blocks;
 
+import mod.syconn.nexus.blockentities.NexusBE;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class NexusBlock extends Block {
+public class NexusBlock extends PipeAttachmentBlock {
 
     public NexusBlock() {
         super(Blocks.IRON_BLOCK.properties());
+    }
+
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return new NexusBE(pPos, pState);
+    }
+
+    public VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        return Shapes.block();
     }
 }
